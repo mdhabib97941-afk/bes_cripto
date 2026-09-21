32cc2df8908cd6843d6a667ec0861d761a281523
mdhabib97941-afk <mdhabib97941@gmail.com>
2026-09-21 19:28:47 +0600

    Delete fix_labels.js

--- fix_labels.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let app = fs.readFileSync('public/app.js', 'utf8');
-
-app = app.replace('Demand: $\\${parseFloat(data.demandVolume).toLocaleString()}', '\\${parseFloat(data.demandVolume).toLocaleString()} Coins');
-app = app.replace('Supply: $\\${parseFloat(data.supplyVolume).toLocaleString()}', '\\${parseFloat(data.supplyVolume).toLocaleString()} Coins');
-
-fs.writeFileSync('public/app.js', app);
-console.log('Fixed Demand/Supply text overlay.');
